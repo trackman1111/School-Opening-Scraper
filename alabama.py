143519796f390c4d26bdb3aad1949c90285b4d4a
rli1215 <56896626+rli1215@users.noreply.github.com>
2021-02-18 21:55:58 -0500

    fixed variable names

--- alabama.py
+++ alabama.py
@@ -12,9 +12,9 @@ def main():
 def to_csv():
     df = pd.DataFrame(columns=['school','alt name','address','instructional delivery','week of','total positive cases','date scraped'])
     url = "https://services7.arcgis.com/4RQmZZ0yaZkGR1zy/arcgis/rest/services/alsde_c19_publish_PUBLIC/FeatureServer/0/query?f=json&where=WeekOf%3D7&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=SchoolSystem%20asc&outSR=102100&resultOffset=0&resultRecordCount=200&resultType=standard&cacheHint=true"
-    openUrl = urllib.request.urlopen(url)
-    jsonD = json.loads(openUrl.read())
-    for p in jsonD["features"]:
+    open_url = urllib.request.urlopen(url)
+    json_data = json.loads(open_url.read())
+    for p in json_data["features"]:
         school_system = p["attributes"]["SchoolSystem"]
         alt_name = p["attributes"]["AltName"]
         address = p["attributes"]["Address"]
