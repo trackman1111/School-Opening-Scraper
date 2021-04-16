f21f45ce81f809379d5e8d729c7732e91f1c15bd
Ian Neidel <68819605+ianneidel@users.noreply.github.com>
2021-04-16 13:00:33 -0700

    Updated row exclusion code, got rid of errors
    Correct date scraped appended to file name, data is up to date for the Monday of the week (likely need to add this in a column or the file name)...

--- new_mexico.py
+++ new_mexico.py
@@ -4,66 +4,56 @@ import os
 import requests
 import csv
 from bs4 import BeautifulSoup
-from datetime import datetime
+from datetime import date
 
 
 
+url = "https://webnew.ped.state.nm.us/"
+page = requests.get(url)
 
-def main():
+url = 'toNMSDP'
+if page.status_code == 200:
+    soup = BeautifulSoup(page.text, 'html5lib')
 
-    url = "https://webnew.ped.state.nm.us/"
-    page = requests.get(url)
+    linkcol = soup.find(class_="col-md-4")
+    for linktxt in linkcol.find_all("a", href=True):
+        if linktxt.contents[0] == 'School Reentry Status':
+            print('linkfound')
+            url = linktxt.get('href')
+else:
+    print("page request error with ", page.status_code)
 
-    url = 'toNMSDP'
-    if page.status_code == 200:
-        soup = BeautifulSoup(page.text, 'html5lib')
+pdfPathName='New_Mexico_SDP.pdf'
+today = date.today()
+finalCSVpath='New_Mexico_'+today.strftime("%m-%d-%y")+'.csv'
+filename = Path(pdfPathName)
+response = requests.get(url)
+filename.write_bytes(response.content)
 
-        linkcol = soup.find(class_="col-md-4")
-        for linktxt in linkcol.find_all("a", href=True):
-            if linktxt.contents[0] == 'School Reentry Status':
-                print('linkfound')
-                url = linktxt.get('href')
-    else:
-        print("page request error with ", page.status_code)
+print('gotpdf')
 
-    pdfPathName = 'New_Mexico_SDP.pdf'
-    finalCSVpath = 'New_Mexico' + datetime.now().strftime('%m-%d-%Y') + '.csv'
-    filename = Path(pdfPathName)
-    response = requests.get(url)
-    filename.write_bytes(response.content)
+tabula.convert_into(pdfPathName, finalCSVpath, output_format="csv", pages='all')
 
-    print('gotpdf')
+print('convertedtocsv')
 
-    tabula.convert_into(pdfPathName, finalCSVpath, output_format="csv", pages='all')
-
-    print('convertedtocsv')
-
-    lines = list()
-    csvhead = ['School district', 'School name', 'Reopening Policy', 'County']
-    lines.append(csvhead)
-    with open(finalCSVpath, 'r') as readFile:
-        reader = csv.reader(readFile)
-        for row in reader:
+lines = list()
+csvhead = ['School district', 'School name', 'Reopening Policy', 'County']
+lines.append(csvhead)
+with open(finalCSVpath, 'r') as readFile:
+    reader = csv.reader(readFile)
+    for row in reader:
+        print(row)
+        if row[0] != "DISTRICT" and row[0] != "":
             lines.append(row)
-            print(row)
-            if row[0] == "DISTRICT":
-                lines.remove(row)
-            elif row[0] == 'Dzit Dit Lool School of Empowerment, Action and':
-                row[0] = 'DEAP SCHOOL'
-            elif row[1] == "":
-                lines.remove(row)
-        print('openedcsv')
+    print('openedcsv')
 
-    with open(finalCSVpath, 'w') as writeFile:
-        writer = csv.writer(writeFile)
-        writer.writerows(lines)
-        print('reformatedcsv')
+with open(finalCSVpath, 'w') as writeFile:
+    writer = csv.writer(writeFile)
+    writer.writerows(lines)
+    print('reformatedcsv')
 
-    try:
-        os.remove(pdfPathName)
-        print('deletedpdf')
-    except:
-        print("could not delete file")
-
-
-main()
+try:
+    os.remove(pdfPathName)
+    print('deletedpdf')
+except:
+    print("could not delete file")
