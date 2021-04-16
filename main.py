cf850fc5dd72eb68e634de79d810c5d9042062ea
Ian Neidel <68819605+ianneidel@users.noreply.github.com>
2021-04-16 13:20:53 -0700

    added new states to main
    error handling?

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import alabama
-import south_carolina
-import tennessee
+import arizona
 import colorado
 import illinois
-import oregon
+import new_mexico
 import maryland
-
+import ohio
+import oregon
+import south_carolina
+import tennessee
+import virginia
+import washington
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     alabama.main()
+    arizona.main()
     colorado.main()
     illinois.main()
+    new_mexico.main()
+    maryland.main()
+    ohio.main()
+    oregon.main()
     south_carolina.main()
     tennessee.main()
-    oregon.main()
-    maryland.main()
+    virginia.main()
+    washington.main()
+    
+
 
 
