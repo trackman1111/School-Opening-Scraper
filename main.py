eb2cd77b1dcf7e2bb0f4ba8e5b91056b5e7f2e58
Griffin Davis <griffincdavis@gmail.com>
2021-04-26 21:34:39 -0500

    Remove Box network logging
    Caused various errors

--- main.py
+++ main.py
@@ -6,26 +6,6 @@ from boxsdk import Client, OAuth2
 from boxsdk.network.default_network import DefaultNetwork
 from pprint import pformat
 
-
-# Logging for Box integration
-class LoggingNetwork(DefaultNetwork):
-    def request(self, method, url, access_token, **kwargs):
-        """ Base class override. Pretty-prints outgoing requests and incoming responses. """
-        print('\x1b[36m{} {} {}\x1b[0m'.format(method, url, pformat(kwargs)))
-        response = super(LoggingNetwork, self).request(
-            method, url, access_token, **kwargs
-        )
-        if response.ok:
-            print('\x1b[32m{}\x1b[0m'.format(response.content))
-        else:
-            print('\x1b[31m{}\n{}\n{}\x1b[0m'.format(
-                response.status_code,
-                response.headers,
-                pformat(response.content),
-            ))
-        return response
-
-
 # Function to handle uploading files to Box
 def uploadToBox(client, folder_id, filename):
     folder = client.folder(folder_id=folder_id)
@@ -66,7 +46,7 @@ if __name__ == '__main__':
             oauth2 = OAuth2(CLIENT_ID, CLIENT_SECRET, access_token=ACCESS_TOKEN)
 
             # Create the authenticated client
-            client = Client(oauth2)#, LoggingNetwork())
+            client = Client(oauth2)
         except Exception as e:
             print("Error creating Box client: %s" % e)
     else:
